be56eba24bd61c6cd7d64ee7c877cbc244434550
JulyBell <julybell16@gmail.com>
2021-07-27 15:14:55 +0300

    closure

--- app.js
+++ app.js
@@ -4,24 +4,28 @@
 		let first = x;
 
 		return function (second){
-			console.log('sum', first + second);
+			return first + second;
 		}
 	}
 
 	const sum1 = sum(1);
 	console.log('first call:');
-	sum1(2);
+	console.log(sum1(2));
 
 	console.log('second call:');
-	sum(1)(2);
+	console.log(sum(1)(2));
 
 	//task 2:
 
 	for (var i = 0; i < 10; i++) {
-		let num = i + 1;
-		setTimeout(function() {
-			console.log(num);
-		}, 0);
+		setTimeout(showRange(i + 1), 0);
 	}
+	
+	function showRange(num){
+		return function(){
+			console.log(num);
+		}
+	}
+
 	/*Ответ на вопрос: функция setTimeout хранит лишь ссылку на переменную i и когда она отработает, цикл уже дойдет до финального значения i=9 (грубо говоря, пока запустится первый таймаут, цикл уже отработается и поэтому 10 раз выведется последнее значение i). 
 	Ну и поскольку цикл у нас заявлен от 0 до 10 (не включая 10), увеличиваем i на 1 и записываем в переменную num.*/
